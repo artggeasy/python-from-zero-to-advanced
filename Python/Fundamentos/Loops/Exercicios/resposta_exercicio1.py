c843fcbd2b8b6ee10d8d59c259782ec69ecae84f
Arthur Araújo <araujo.arthur@acad.ifma.edu.br>
2026-04-19 20:42:02 -0300

    commit changes exercicios

---
+++ Python/Fundamentos/Loops/Exercicios/resposta_exercicio1.py
@@ -0,0 +1,11 @@
+print('\nContando de 1 a 10 usando o FOR')
+for i in range(1,11):
+    print(i)
+
+
+print("\nContando de 1 a 10 usando o WHILE")
+#ou com while
+contador = 1
+while contador !=11:
+    print(contador)
+    contador = contador + 1
---
+++ Python/Fundamentos/Loops/loop_while.py
@@ -0,0 +1,6 @@
+contador = 0
+while contador < 5:
+    print("Olá, mundo!")
+    contador += 1
+#Enquanto o contador for menor que 5, ele vai imprimir "Olá, Mundo!" e depois incrementar 1 no contador ate que o contador seja 5
+
