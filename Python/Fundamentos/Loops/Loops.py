f7fa02f22d6dab1905695d018034a7b65c5890c7
Arthur Araújo <araujo.arthur@acad.ifma.edu.br>
2026-04-19 12:38:36 -0300

    loops1edit

--- Python/Fundamentos/Loops/Loops.py
+++ Python/Fundamentos/Loops/Loops.py
@@ -3,5 +3,11 @@ for i in range(5):
     print("Olá, mundo!")
     
 #de 1 até 5 (sempre começa do 1)
+print("\n")
 for i in range(1, 6): #de 1 até 5
+    print(f"Olá, mundo! {i}")
+
+print("\n")
+#outro exemplo de range com "i"
+for i in range(1, 11):
     print(i)
