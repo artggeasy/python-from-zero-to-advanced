47f66e3c0c6467b2e9357d385f5bff1659183501
Arthur <84977511+artggeasy@users.noreply.github.com>
2026-04-22 13:55:05 +0000

    atividade2_resolvida

--- Python Nivel 2/Listas e Dicionarios/Exercicios/exercios2.py
+++ Python Nivel 2/Listas e Dicionarios/Exercicios/exercios2.py
@@ -1,1 +1,11 @@
+def resumo_usuario(usuario):
+    return f"{usuario["Nome"]} tem {usuario["Idade"]} anos e mora em {usuario["Cidade"]}"
+
+
+usuario1 = {"Nome":"Arthur","Idade":22,"Cidade":"Caxias"}
+usuario2 = {"Nome":"Sabrina","Idade": 20,"Cidade":"Açailandia"}
+
+print(resumo_usuario(usuario1))
+print(resumo_usuario(usuario2))
+
 
